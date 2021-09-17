2811f0ee86b7af319140e0aec7b7a7e72cbecf53
DamirNightCoder89 <keyn_89@mail.ru>
2021-09-17 22:46:29 +0300

    from scratch

---
+++ shareservice/src/main/java/com/damirkan/shareservice/ShareserviceApplication.java
@@ -0,0 +1,13 @@
+package com.damirkan.shareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareserviceApplication.class, args);
+	}
+
+}
---
+++ shareservice/src/test/java/com/damirkan/shareservice/ShareserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damirkan.shareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
