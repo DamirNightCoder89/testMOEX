959560427663942a61ffd39758e3a525cac763eb
DamirNightCoder89 <keyn_89@mail.ru>
2021-09-18 22:01:30 +0300

    some work

--- shareservice/src/main/java/com/damirkan/shareservice/ShareserviceApplication.java
+++ shareservice/src/main/java/com/damirkan/shareservice/ShareserviceApplication.java
@@ -3,6 +3,7 @@ package com.damirkan.shareservice;
 import com.damirkan.shareservice.model.Share;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,16 +24,18 @@ public class ShareserviceApplication {
 
 	}
 
-	String uri = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json?iss.meta=off&iss.only=marketdata&marketdata.columns=SECID,LAST";
+	String uri = "https://iss.moex.com/iss/engines/stock/markets/shares/securities.json?iss.meta=off&iss.version=off&iss.json=extended&iss.only=marketdata&marketdata.columns=SECID,LAST";
 	WebClient webClient = WebClient.create();
 
 	@GetMapping("/tr")
-	public  void retrive() {
+	public  Mono<Object[]> retrive() {
 		Mono<Object[]> shareMono = webClient.get()
 				.uri(uri)
+				.accept(MediaType.APPLICATION_JSON)
 				.retrieve()
 				.bodyToMono(Object[].class).log();
 		System.out.println("yeu");
+		return shareMono;
 
 	}
 
