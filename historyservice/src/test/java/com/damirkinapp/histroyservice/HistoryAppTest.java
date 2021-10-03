1c591e95e4439e280376e1e23909f82b988eed16
DamirNightCoder89 <keyn_89@mail.ru>
2021-10-03 23:26:03 +0300

    some work

---
+++ historyservice/src/test/java/com/damirkinapp/histroyservice/HistoryAppTest.java
@@ -0,0 +1,20 @@
+package com.damirkinapp.histroyservice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HistoryAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
